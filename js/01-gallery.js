0a0bf19b01dd0a073dbab68e173555d668976330
DmitriyShevchenko23 <dmitryshevchenko23@gmail.com>
2022-07-16 21:56:42 +0500

    сделал разметку галереи

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkkUp = createGalleryMarkUp(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend',galleryMarkkUp)
+
+galleryContainer.addEventListener('click',onGalleryContainerClick)
+
+function createGalleryMarkUp(galleryItems) {
+    return galleryItems
+        .map((item) => {
+            return `<div class='gallery__item'>
+            <a
+            class='gallery__link'
+            href='${item.original}'>
+            <img
+            class='gallery__img'
+            alt='${item.description}' 
+            src='${item.preview}'>
+            </a>
+         </div>`;     
+    })
+    .join('');  
+    
+};
+
+function onGalleryContainerClick(event) {
+    const imageEl = event.target.dataset.original;
+    if (!imageEl) {
+        return;
+    }
+};
