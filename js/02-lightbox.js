d99dfda972a410939313bee35c20c48ec648f5de
DmitriyShevchenko23 <dmitryshevchenko23@gmail.com>
2022-07-17 21:51:56 +0500

    task 07-02 almost done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkUp = createGalleryMarkUp(galleryItems)
+
+galleryContainer.insertAdjacentHTML('beforeend',galleryMarkUp)
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createGalleryMarkUp(galleryItems) {
+    return galleryItems.map(({ original, preview, description }) => {
+        return `
+       <div class="gallery__item">
+  <a class="gallery__item" 
+     href="${original}">
+    <img 
+    class="gallery__image" 
+    src="${preview}" 
+    alt="${description}"
+    data-source='${original}' 
+    />
+  </a>
+</div>`
+    }).join('')
+}
+
+function onGalleryContainerClick(event)  {
+    event.preventDefault()
+
+    const imageEl = event.target.dataset.source;
+
+    if (!imageEl) {
+        return;
+    } else {
+        const lightbox = new SimpleLightbox(".gallery a", {
+            captionsData: "alt",
+            captionDelay: 250,
+            captionPosition: "bottom",
+        });
+    };
+};
+
+
+
+
